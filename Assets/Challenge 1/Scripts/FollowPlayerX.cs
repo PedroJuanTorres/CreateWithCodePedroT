c621bd402e0a4030af86fb7b92e865cc3ffeeaa9
PedroJTorres <pedrojuantorres.inforgmail.com>
2022-01-25 14:31:07 +0000

    Complete challenge one

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    [SerializeField]private GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(42.0f, 0.0f, 2.2f);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
